d1f1d9bc6f13f84a5e0a19bdaac4a320c81a4d53
petikdua-code <petikdua-code@users.noreply.github.com>
2020-05-19 01:59:03 +0700

    start

---
+++ app/Http/Controllers/Auth.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class Auth extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login/index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $email = $request->email;
+        $password = $request->password;
+
+        $data = User::where('email', $email)->first();
+        if($data) {
+            if($password == $data->password) {
+                if($data->admin == 0){
+                    return redirect('/user')->with('status', 'Data sipp joss');
+                    // return "user biasa";
+                } elseif($data->admin == 1) {
+                    return "admin super";
+                }
+            } else {
+                return "pass salah";
+            }
+        } else {
+            return "email nggk ada";
+        }
+
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,59 @@
+@extends('templates/main')
+
+@section('title', 'Login Campuspedia')
+
+@section('container')
+  <div class="container mt-2">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-lg-6">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+              <div class="col-lg">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                  </div>
+
+                  <form class="user" method="post" action="/auth">
+                    @csrf
+                    <div class="form-group">
+                      <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="email" value="">
+                      
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
+                   
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </button>
+
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  @endsection
